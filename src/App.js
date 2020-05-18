fcee1c9492dd19983a1dd2900e3f526533b5014e
Kushagra <kush12092001@gmail.com>
2020-05-18 13:30:43 +0530

    Presentational and Container Components

--- src/App.js
+++ src/App.js
@@ -1,27 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import { Navbar } from 'reactstrap';
-import { NavbarBrand } from 'reactstrap';
-import Menu from './components/Menucomponent';
-import { DISHES } from './Shared/dishes';
+import './App.css';
+import Main from './components/MainComponent';
+
 
 class App extends Component {
-  constructor(props){
-    super(props);
-
-    this.state = {
-      dishes: DISHES
-    };
-  }
+  
   render(){
   return (
     <div className="App">
-      <Navbar dark color='primary'>
-        <div className="container">
-          <NavbarBrand href="/">Ristornte Con Fusion</NavbarBrand>
-        </div>
-      </Navbar>
-      <Menu dishes={this.state.dishes} />
+      <Main />
     </div>
     );
   }
--- src/components/Menucomponent.js
+++ src/components/Menucomponent.js
@@ -12,15 +12,11 @@ class Menu extends Component {
         }
     }
 
-    onDishSelect(dish) {
-        this.setState({ selectedDish: dish });
-    }
-
     render() {
         const menu = this.props.dishes.map(dish => {
             return (
                 <div key={dish.id} className="col-12 col-md-5 m-1">
-                    <Card onClick={() => this.onDishSelect(dish)}>
+                    <Card onClick={() => this.props.onClick(dish.id)}>
                         <CardImg width="100%" src={dish.image} alt={dish.name} />
                         <CardImgOverlay>
                             <CardTitle>{dish.name}</CardTitle>
@@ -35,7 +31,6 @@ class Menu extends Component {
                 <div className="row">
                     {menu}
                 </div>
-                <DishDetail dish={this.state.selectedDish} />
             </div>
         );
     }
